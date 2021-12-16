04e5f12b84583cc403cca72e6a501704d20c5db9
Jerimiah <JerimiahOfficial@gmail.com>
2021-12-16 14:31:11 -0500

    Made the population of array randomized

--- ProgrammingProblems/Arrays.cpp
+++ ProgrammingProblems/Arrays.cpp
@@ -13,6 +13,11 @@
 		from the array at its respected
 		index.
 
+	5.	Add a duplicate number to the 
+		array.
+
+	6.	Find the duplicate number from
+		the array and remove it.
 */
 
 #include "Arrays.h"
@@ -20,13 +25,26 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <time.h>
+
+int random() {
+	// Create a random number.
+	std::srand(static_cast<unsigned int>(time(NULL)));
+	return std::rand() % 50 + 2;
+}
+
+int randomInRange(int size) {
+	// Create a random number.
+	std::srand(static_cast<unsigned int>(time(NULL)));
+	return std::rand() % size + 1;
+}
 
 void Arrays::BasicOperations() {
 	// Initialize an empty array.
 	std::vector<int> Numbers;
 
-	// Add numbers 1-10 to an array.
-	for (int i = 0; i < 10; i++) {
+	// Add numbers to an array.
+	for (int i = 0; i < random(); i++) {
 		Numbers.push_back(i + 1);
 	}
 
@@ -36,14 +54,14 @@ void Arrays::BasicOperations() {
 	std::cout << std::endl << std::endl;
 
 	// Remove a number from the array.
-	Numbers.erase(Numbers.begin() + 4);
+	Numbers.erase(Numbers.begin() + randomInRange(static_cast<int>(Numbers.size())));
 
 	// Print out the array.
 	std::cout << "Array after number removed:" << std::endl;
 	std::copy(Numbers.begin(), Numbers.end(), std::ostream_iterator<int>(std::cout, " "));
 	std::cout << std::endl;
 
-	// Find missing number from 1-10 
+	// Find missing number from the array
 	// and add it back into the array.
 	for (int i = 0; i < static_cast<int>(Numbers.size()); i++) {
 		if (i + 1 != Numbers[i]) {
@@ -54,6 +72,28 @@ void Arrays::BasicOperations() {
 
 	// Print out the array.
 	std::cout << "Array after number added back:" << std::endl;
+	std::copy(Numbers.begin(), Numbers.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cout << std::endl << std::endl;
+
+	// Add duplicate number into array.
+	Numbers.insert(Numbers.begin() + randomInRange(static_cast<int>(Numbers.size())), randomInRange(static_cast<int>(Numbers.size())));
+
+	// Print out the array.
+	std::cout << "Array after duplicate number added:" << std::endl;
+	std::copy(Numbers.begin(), Numbers.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cout << std::endl;
+
+	// Find duplicate number the array 
+	// and add it back into the array.
+	for (int i = 0; i < static_cast<int>(Numbers.size()); i++) {
+		if (i + 1 != Numbers[i]) {
+			std::cout << std::endl << i << " is duplicate number in the Number array." << std::endl << std::endl;
+			Numbers.erase(Numbers.begin() + i);
+		}
+	}
+
+	// Print out the array.
+	std::cout << "Array after duplicate number removed:" << std::endl;
 	std::copy(Numbers.begin(), Numbers.end(), std::ostream_iterator<int>(std::cout, " "));
 	std::cout << std::endl;
 }
