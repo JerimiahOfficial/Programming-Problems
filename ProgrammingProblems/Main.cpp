23cbabe5c1e81f0150f156a418fde3a4542022b7
Jerimiah <JerimiahOfficial@gmail.com>
2021-12-16 13:15:25 -0500

    Add project files.

---
+++ ProgrammingProblems/Main.cpp
@@ -0,0 +1,56 @@
+/*
+
+	Programming problems
+
+	1.	Add numbers to an empty array.
+
+	2.	Remove a number from an array.
+
+	3.	Find the number that was removed.
+
+	4.	Add the number that was removed 
+		from the array.
+
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+	// Initialize an empty array.
+	std::vector<int> Numbers;
+
+	// Add numbers 1-10 to an array.
+	for (int i = 0; i < 10; i++) {
+		Numbers.push_back(i + 1);
+	}
+
+	// Print out the array.
+	std::cout << "Array after numbers added:" << std::endl;
+	std::copy(Numbers.begin(), Numbers.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cout << std::endl;
+
+	// Remove a number from the array.
+	Numbers.erase(Numbers.begin() + 4);
+
+	// Print out the array.
+	std::cout << "Array after number removed:" << std::endl;
+	std::copy(Numbers.begin(), Numbers.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cout << std::endl;
+
+	// Find missing number from 1-10 
+	// and add it back into the array.
+	for (int i = 0; i < 10; i++) {
+		if (i + 1 != Numbers[i]) {
+			std::cout << std::endl << i + 1 << " is missing from Number array." << std::endl << std::endl;
+			Numbers.insert(Numbers.begin() + i, { i + 1 });
+		}
+	}
+
+	// Print out the array.
+	std::cout << "Array after number added back:" << std::endl;
+	std::copy(Numbers.begin(), Numbers.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cout << std::endl;
+}
